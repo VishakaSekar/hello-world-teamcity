73f8792cea3250e3588eb846f9eaef82992e12e6
admin <admin@TeamCity>
2019-01-25 11:01:02 -0800

    TeamCity change in 'Hello World Teamcity' project: 9 build step(s) were added to 'Build' build configuration

--- .teamcity/HelloWorldTeamcity/buildTypes/HelloWorldTeamcity_Build.kt
+++ .teamcity/HelloWorldTeamcity/buildTypes/HelloWorldTeamcity_Build.kt
@@ -1,6 +1,10 @@
 package HelloWorldTeamcity.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.v2018_2.*
+import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.dockerCommand
+import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.dockerCompose
+import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.maven
+import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.v2018_2.triggers.vcs
 
 object HelloWorldTeamcity_Build : BuildType({
@@ -9,6 +13,43 @@ object HelloWorldTeamcity_Build : BuildType({
 
     vcs {
         root(HelloWorldTeamcity.vcsRoots.HelloWorldTeamcity_HttpsGithubComVishakaSekarKafkaMonitoringExtensionCiRefsHeads)
+    }
+
+    steps {
+        dockerCompose {
+            file = "docker-compose.yml"
+        }
+        dockerCommand {
+            commandType = build {
+                source = path {
+                    path = "Dockerfile"
+                }
+            }
+        }
+        maven {
+            goals = "clean test"
+            runnerArgs = "-Dmaven.test.failure.ignore=true"
+            mavenVersion = defaultProvidedVersion()
+            jdkHome = "%env.JDK_18%"
+        }
+        script {
+            scriptContent = "./broker-list.sh"
+        }
+        script {
+            scriptContent = "./create-topics.sh"
+        }
+        script {
+            scriptContent = "./download-kafka.sh"
+        }
+        script {
+            scriptContent = "./start-kafka-shell.sh"
+        }
+        script {
+            scriptContent = "./start-kafka.sh"
+        }
+        script {
+            scriptContent = "./versions.sh"
+        }
     }
 
     triggers {
